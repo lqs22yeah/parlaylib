e8a7a41c6590da47548075e59e6b72a271e0f592
lqs22yeah <lqs22@mails.tsinghua.edu.cn>
2024-12-09 17:27:06 +0800

    ...

---
+++ examples/mergesort_skew.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <random>
+
+#include <parlay/io.h>
+#include <parlay/primitives.h>
+#include <parlay/random.h>
+#include <parlay/sequence.h>
+
+#include "mergesort.h"
+
+// **************************************************************
+// Driver
+// **************************************************************
+std::vector<long> generate_exponential_weights(int n,double lambda){
+  std::vector<long> weights(n);
+  for (int i=0;i<n;i++){
+    weights[i]=static_cast<long>(exp(-lambda*i));
+  }
+  return weights;
+}
+int main(int argc, char* argv[]) {
+  auto usage = "Usage: mergesort <n>";
+  if (argc != 2) std::cout << usage << std::endl;
+  else {
+    long n;
+    try { n = std::stol(argv[1]); }
+    catch (...) { std::cout << usage << std::endl; return 1; }
+    parlay::random_generator gen;
+    double lambda=0.01;
+    auto weights=generate_exponential_weights(n,lambda);
+    std::discrete_distribution<long> dis(weights.begin(),weights.end());
+    
+
+    // generate random long values
+    auto data = parlay::tabulate(n, [&] (long i) {
+      auto r = gen[i];
+      return dis(r);});
+
+    parlay::internal::timer t("Time");
+    parlay::sequence<long> result;
+    for (int i=0; i < 5; i++) {
+      result = data;
+      t.start();
+      merge_sort(result);
+      t.next("mergesort");
+    }
+
+    auto first_ten = result.head(10);
+
+    std::cout << "first 10 elements: " << parlay::to_chars(first_ten) << std::endl;
+  }
+}
+
