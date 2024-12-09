8fde9aeea7b01db9e478b96647f279be7ffb4bab
lqs22yeah <lqs22@mails.tsinghua.edu.cn>
2024-12-09 23:55:26 +0800

    ...

--- examples/mergesort_skew.cpp
+++ examples/mergesort_skew.cpp
@@ -12,12 +12,27 @@
 // **************************************************************
 // Driver
 // **************************************************************
-std::vector<long> generate_exponential_weights(int n,double lambda){
-  std::vector<long> weights(n);
-  for (int i=0;i<n;i++){
-    weights[i]=static_cast<long>(10000000*exp(-lambda*i));
-  }
-  return weights;
+std::vector<long> generate_zipf_weights(int n, double s) {
+    std::vector<long> weights(n);
+    
+    // Sum of the values to normalize later
+    double sum = 0;
+    
+    // Calculate raw Zipf values for each index
+    for (int i = 1; i <= n; ++i) {
+        weights[i-1] = static_cast<long>(1000000*pow(i, -s));
+        sum += weights[i-1];
+    }
+    
+    // // Normalize the weights so they sum to 1
+    // for (int i = 0; i < n; ++i) {
+    //     weights[i] = static_cast<long>(weights[i] / sum * 10000000);
+    // }
+    std::random_device rd;          // 1
+    std::mt19937 g(rd());           // 2
+    std::shuffle(weights.begin(), weights.end(), g); // 3
+
+    return weights;
 }
 int main(int argc, char* argv[]) {
   auto usage = "Usage: mergesort <n>";
@@ -27,8 +42,8 @@ int main(int argc, char* argv[]) {
     try { n = std::stol(argv[1]); }
     catch (...) { std::cout << usage << std::endl; return 1; }
     parlay::random_generator gen;
-    double lambda=0.01;
-    auto weights=generate_exponential_weights(n,lambda);
+    double s=1;
+    auto weights = generate_zipf_weights(n, s);
     std::discrete_distribution<long> dis(weights.begin(),weights.end());
     
 
@@ -36,7 +51,7 @@ int main(int argc, char* argv[]) {
     auto data = parlay::tabulate(n, [&] (long i) {
       auto r = gen[i];
       return dis(r);});
-
+    
     parlay::internal::timer t("Time");
     parlay::sequence<long> result;
     for (int i=0; i < 5; i++) {
